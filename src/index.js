b518a402646d9d0d0fdcd821b38057c9348097bb
CorradoRossi <rossimeacham@gmail.com>
2019-04-25 22:29:46 -0400

    add favicon

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './stylesheets/spur.scss'
+import './stylesheets/dolphin.scss'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
