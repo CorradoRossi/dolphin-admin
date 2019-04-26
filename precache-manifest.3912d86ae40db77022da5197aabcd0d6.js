0ca1e5ff21ebb4b7aaea443d469c24e9a198666e
CorradoRossi <rossimeacham@gmail.com>
2019-04-25 22:00:28 -0400

    Updates

---
+++ precache-manifest.3912d86ae40db77022da5197aabcd0d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c0d5c25ba43267e156366f2fb03ac8a",
+    "url": "/dolphin-admin/static/media/logo.0c0d5c25.svg"
+  },
+  {
+    "revision": "52666d4816c4334874e2",
+    "url": "/dolphin-admin/static/js/runtime~main.4eec82db.js"
+  },
+  {
+    "revision": "b026852e1cb9db5bea4a",
+    "url": "/dolphin-admin/static/js/main.7dabd5c4.chunk.js"
+  },
+  {
+    "revision": "cce5a3e0627ff89e413a",
+    "url": "/dolphin-admin/static/js/2.d3b79b33.chunk.js"
+  },
+  {
+    "revision": "b026852e1cb9db5bea4a",
+    "url": "/dolphin-admin/static/css/main.92a6f509.chunk.css"
+  },
+  {
+    "revision": "22844bd05c5438ee4dc02a0c6f931a47",
+    "url": "/dolphin-admin/index.html"
+  }
+];
