6c6fa1995a85480b9382f04c6b0a24cf696fc769
CorradoRossi <rossimeacham@gmail.com>
2019-04-27 22:13:15 -0400

    change routes to accomodate Github pages

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ import Content from './views/examples/Content';
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
-            <Route path="dolphin-admin/home" render={props => <AdminLayout {...props} />} />
-            <Route path="dolphin-admin/auth" render={props => <AuthLayout {...props} />} />
-            <Route path="dolphin-admin/charts" render={props => <ChartsPage {...props} />} />
-            <Route path="dolphin-admin/content" render={props => <Content {...props} />} />
-            <Redirect from="/" to="dolphin-admin/home" />
+            <Route path="/dolphin-admin/home" render={props => <AdminLayout {...props} />} />
+            <Route path="/dolphin-admin/auth" render={props => <AuthLayout {...props} />} />
+            <Route path="/dolphin-admin/charts" render={props => <ChartsPage {...props} />} />
+            <Route path="/dolphin-admin/content" render={props => <Content {...props} />} />
+            <Redirect from="/" to="/dolphin-admin/home" />
         </Switch>
     </BrowserRouter>,
 document.getElementById('root')
--- src/routes.js
+++ src/routes.js
@@ -6,25 +6,25 @@ import Content from "./views/examples/Content.jsx";
 
 var routes = [
     {
-        path: 'dolphin-admin/home',
+        path: '/dolphin-admin/home',
         name: 'Home',
         component: AdminLayout,
         layout: '/admin'
     },
     {
-        path: 'dolphin-admin/login',
+        path: '/dolphin-admin/login',
         name: 'auth',
         component: AuthLayout,
         layout: '/auth'
     },
     {
-        path: 'dolphin-admin/charts',
+        path: '/dolphin-admin/charts',
         name: 'charts',
         component: ChartsPage,
         layout: '/admin'
     },
     {
-        path: 'dolphin-admin/content',
+        path: '/dolphin-admin/content',
         name: 'content',
         component: Content,
         layout: '/admin'
